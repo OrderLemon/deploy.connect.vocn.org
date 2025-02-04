6afbbf6574e6ed5016040d7353f4f015141c5c99
Axel Musch <110383963+axelmuschfabulor@users.noreply.github.com>
2025-02-04 09:13:04 +0100

    Create webhook_script.js

---
+++ webhook_script.js
@@ -0,0 +1,55 @@
+class Script {
+  /**
+   * @params {object} request
+   */
+  process_incoming_request({ request }) {
+    // request.url.hash
+    // request.url.search
+    // request.url.query
+    // request.url.pathname
+    // request.url.path
+    // request.url_raw
+    // request.url_params
+    // request.headers
+    // request.user._id
+    // request.user.name
+    // request.user.username
+    // request.content_raw
+    // request.content
+
+    // console is a global helper to improve debug
+    console.log(request.content);
+    const name_split = request.content.ReferringPhysiciansName.split(",")
+    let username
+    if (name_split.length > 1) {
+      username = name_split[0] + "." + name_split[1]
+    } else {
+      username = name_split[0]
+    }
+
+    return {
+      content: {
+        text: request.content.text,
+        channel: "@" + username,
+        alias: "Vetrocket",
+        avatar: "https://picsum.photos/id/40/200",
+        // "attachments": [{
+        //   "color": "#FF0000",
+        //   "author_name": "Rocket.Cat",
+        //   "author_link": "https://open.rocket.chat/direct/rocket.cat",
+        //   "author_icon": "https://open.rocket.chat/avatar/rocket.cat.jpg",
+        //   "title": "Rocket.Chat",
+        //   "title_link": "https://rocket.chat",
+        //   "text": "Rocket.Chat, the best open source chat",
+        //   "fields": [{
+        //     "title": "Priority",
+        //     "value": "High",
+        //     "short": false
+        //   }],
+        //   "image_url": "https://rocket.chat/images/mockup.png",
+        //   "thumb_url": "https://rocket.chat/images/mockup.png"
+        // }]
+      }
+    }
+  }
+}
