f68dcdd713c4abda46675d52d57b6f3a8d65e0a2
Axel Musch <110383963+axelmuschfabulor@users.noreply.github.com>
2025-02-26 16:33:58 +0100

    Update webhook_script.js

--- webhook_script.js
+++ webhook_script.js
@@ -1,55 +1,61 @@
 class Script {
-  /**
-   * @params {object} request
-   */
-  process_incoming_request({ request }) {
-    // request.url.hash
-    // request.url.search
-    // request.url.query
-    // request.url.pathname
-    // request.url.path
-    // request.url_raw
-    // request.url_params
-    // request.headers
-    // request.user._id
-    // request.user.name
-    // request.user.username
-    // request.content_raw
-    // request.content
+    /**
+     * @params {object} request
+     */
+    process_incoming_request({ request }) {
+        // request.url.hash
+        // request.url.search
+        // request.url.query
+        // request.url.pathname
+        // request.url.path
+        // request.url_raw
+        // request.url_params
+        // request.headers
+        // request.user._id
+        // request.user.name
+        // request.user.username
+        // request.content_raw
+        // request.content
 
-    // console is a global helper to improve debug
-    console.log(request.content);
-    const name_split = request.content.ReferringPhysiciansName.split(",")
-    let username
-    if (name_split.length > 1) {
-      username = name_split[0] + "." + name_split[1]
-    } else {
-      username = name_split[0]
-    }
+        // console is a global helper to improve debug
+        console.log(request.content);
+        const patient = request.content.PatientsName.split(",").reverse().join(" ")
+        const url = request.content.ReportOriginalUrl
+        const stat = request.content.StatRequest
+        const species = request.content.PatientSpeciesDescription
+        const images = request.content.NumberOfStudyRelatedInstances
+        const modality = request.content.ModalitiesInStudy
 
-    return {
-      content: {
-        text: request.content.text,
-        channel: "@" + username,
-        alias: "Vetrocket",
-        avatar: "https://picsum.photos/id/40/200",
-        // "attachments": [{
-        //   "color": "#FF0000",
-        //   "author_name": "Rocket.Cat",
-        //   "author_link": "https://open.rocket.chat/direct/rocket.cat",
-        //   "author_icon": "https://open.rocket.chat/avatar/rocket.cat.jpg",
-        //   "title": "Rocket.Chat",
-        //   "title_link": "https://rocket.chat",
-        //   "text": "Rocket.Chat, the best open source chat",
-        //   "fields": [{
-        //     "title": "Priority",
-        //     "value": "High",
-        //     "short": false
-        //   }],
-        //   "image_url": "https://rocket.chat/images/mockup.png",
-        //   "thumb_url": "https://rocket.chat/images/mockup.png"
-        // }]
-      }
+        let prio_text = ''
+        switch (stat) {
+            case "0":
+            case "00":
+            case 0:
+                break;
+            case "10":
+            case 10:
+                prio_text = "12hr"
+                break;
+            case "20":
+            case 20:
+                prio_text = "1hr"
+                break;
+            case "30":
+            case 30:
+                prio_text = "2hr"
+                break;
+            default:
+                prio_text = "Undefined priority"
+        }
+        if (prio_text === '') {
+            return
+        } else {
+            return {
+                content: {
+                    text: `${prio_text} \`${modality}\` STAT requested for ${patient}${species ? "(" + species + ")" : ""} \n [Click here to view the images(${images})](${url})`,
+
+                }
+            }
+        }
     }
-  }
 }
